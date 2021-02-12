4874c2ce2e0c9e368c0b36dd5d1ec7ef5317ac00
Pablo Cabrera <3091865790609@ingenieria.usac.edu.gt>
2021-02-12 01:31:49 -0600

    =Inicio del proyecto, gramatica inicial

---
+++ Regexive/src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+import GUI.MainGUI;
+
+/**
+ *
+ * @author pablo
+ */
+public class Main {
+
+    public static MainGUI gui = new MainGUI();
+
+    public static void main(String[] args) {
+        gui.setVisible(true);
+    }
+
+}
